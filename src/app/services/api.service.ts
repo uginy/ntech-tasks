a125d306482e8588e393598c6a06e770e24386e7
Ievgeny Khoptiar <ievgen.khoptiar@gmail.com>
2019-06-07 17:09:29 +0300

    api fix

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { sourceUrl } from '../../assets/config';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +16,7 @@ export class ApiService {
     this.base = sourceUrl;
   }
   private base: string;
-  private responseCache = new Map();
+  // private responseCache = new Map();
 
   public httpOptions = {
     headers: new HttpHeaders({
@@ -20,18 +25,45 @@ export class ApiService {
     })
   };
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code. The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError(error);
+  }
+
+  private extractData(res: Response) {
+    const body = res;
+    return body || {};
+  }
+
   private getUrl(url: string = ''): string {
     return this.base + url;
   }
 
-  // Caching for HTTP requests
   public getDataHttp(url: string): Observable<any> {
-    const dataCache = this.responseCache.get(this.getUrl(url));
-    if (dataCache) {
-      return of(dataCache);
-    }
-    const response = this.http.get<[]>(this.getUrl(url), this.httpOptions);
-    response.subscribe(data => this.responseCache.set(this.getUrl(url), data));
-    return response;
+    // Call the http GET
+    return this.http.get(this.getUrl(url), this.httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
   }
+
+  // Caching for HTTP requests
+  // public getDataHttp(url: string): Observable<any> {
+  //   const dataCache = this.responseCache.get(this.getUrl(url));
+  //   if (dataCache) {
+  //     return of(dataCache);
+  //   }
+  //   const response = this.http.get<[]>(this.getUrl(url), this.httpOptions);
+  //   response.subscribe(data => this.responseCache.set(this.getUrl(url), data));
+  //   return response;
+  // }
 }
